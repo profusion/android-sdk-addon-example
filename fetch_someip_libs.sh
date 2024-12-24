b5d1c368f507ead90d7ee012268d2a62ea680705
MaironLucas <mairon.slusarz@profusion.mobi>
2024-12-24 15:28:44 -0300

    feat: create a SOME/IP service and client
    - Create a simple HelloWorld service and client using CommonAPI

--- fetch_someip_libs.sh
+++ fetch_someip_libs.sh
@@ -1,11 +1,27 @@
 
 echo "Clonning SOME/IP libs"
-if [ ! -d libs/temp/capicxx-someip-runtime ]; then
-    git clone https://github.com/COVESA/capicxx-someip-runtime.git libs/temp/capicxx-someip-runtime
+if [ ! -d "libs/temp" ]; then
+    mkdir libs/temp
 fi
-if [ ! -d libs/temp/capicxx-core-runtime ]; then
-    git clone https://github.com/COVESA/capicxx-core-runtime.git libs/temp/capicxx-core-runtime
+cd libs/temp
+if [ ! -d "capicxx-someip-runtime" ]; then
+    git clone https://github.com/COVESA/capicxx-someip-runtime.git capicxx-someip-runtime
 fi
-if [ ! -d libs/temp/boost ]; then
-    git clone https://github.com/boostorg/log.git libs/temp/boost/log
+if [ ! -d "capicxx-core-runtime" ]; then
+    git clone https://github.com/COVESA/capicxx-core-runtime.git capicxx-core-runtime
+fi
+if [ ! -d "boost" ]; then
+    git clone https://github.com/boostorg/log.git boost/log
+fi
+cd ..
+mkdir -p generators
+if [ ! -d "generators/commonapi_someip_generator" ]; then
+    wget https://github.com/COVESA/capicxx-someip-tools/releases/download/3.2.15/commonapi_someip_generator.zip
+    unzip commonapi_someip_generator.zip -d generators/commonapi_someip_generator
+    rm commonapi_someip_generator.zip
+fi
+if [ ! -d "generators/commonapi_core_generator" ]; then
+    wget https://github.com/COVESA/capicxx-core-tools/releases/download/3.2.15/commonapi_core_generator.zip
+    unzip commonapi_core_generator.zip -d generators/commonapi_core_generator
+    rm commonapi_core_generator.zip
 fi
---
+++ packages/services/profusion/test-someip-service/src/HelloWorldStubImpl.hpp
@@ -0,0 +1,13 @@
+#ifndef HELLOWORLDSTUBIMPL_H_
+#define HELLOWORLDSTUBIMPL_H_
+#include <CommonAPI/CommonAPI.hpp>
+#include <v1/com/profusion/helloWorldSomeIp/HelloWorldStubDefault.hpp>
+
+class HelloWorldStubImpl: public v1_0::com::profusion::helloWorldSomeIp::HelloWorldStubDefault {
+public:
+    HelloWorldStubImpl();
+    virtual ~HelloWorldStubImpl();
+    virtual void sayHello(const std::shared_ptr<CommonAPI::ClientId> _client,
+    	std::string _name, sayHelloReply_t _return);
+};
+#endif /* HELLOWORLDSTUBIMPL_H_ */
